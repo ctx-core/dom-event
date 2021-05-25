4156d418a04f402679a4934df253047f1915c3a0
Brian Takita <brian.takita@gmail.com>
2021-05-25 01:29:10 -0400

    ## 6.3.0
    ### Minor Changes
    
    - fix tsc build directory issues

--- src/index.js
+++
@@ -1,3 +0,0 @@
-export * from './_MouseEvent';
-export * from './trigger_custom_event';
-export * from './trigger_native_event';
--- src/trigger_custom_event.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function trigger_custom_event<T>(el: HTMLElement, event_name: string, detail: T): void;
-export { trigger_custom_event as trigger__event__custom };
