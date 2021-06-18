5ee36f855f3c4fcb50d83f3d78ea85ff1d8a3411
Brian Takita <brian.takita@gmail.com>
2021-06-18 03:47:36 -0400

    remove src/*.js & src/*.map

--- src/index.js
+++
@@ -1,4 +0,0 @@
-export * from './mouse_event_';
-export * from './trigger_custom_event';
-export * from './trigger_native_event';
-//# sourceMappingURL=src/index.js.map
--- src/trigger_native_event.js
+++
@@ -1,7 +0,0 @@
-export function trigger_native_event(el, event_name) {
-    const event = document.createEvent('HTMLEvents');
-    event.initEvent(event_name, true, false);
-    el.dispatchEvent(event);
-}
-export { trigger_native_event as trigger__event__native, };
-//# sourceMappingURL=src/trigger_native_event.js.map
