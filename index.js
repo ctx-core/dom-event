44e4f7c3254cd7ff8f68fe0524eb960506dee459
Brian Takita <brian.takita@gmail.com>
2023-11-11 17:35:56 -0500

    @@ -1,5 +1,19 @@ # @ctx-core/event
    ## 9.2.0
    
    ### Minor Changes
    
    - tsconfig:
    
    "module": "ESNext"
    "moduleResolution": "nodenext"
    "target": "ESNext"
    
    ### Patch Changes
    
    - package.json: - "svelte": "./src/index.js"
    
    ## 9.1.21
    
    ### Patch Changes

---
+++ index.js
@@ -0,0 +1,3 @@
+export * from './custom_event__trigger/index.js'
+export * from './mouse_event_/index.js'
+export * from './native_event__trigger/index.js'
--- src/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './custom_event__trigger'
-export * from './mouse_event_'
-export * from './native_event__trigger'
